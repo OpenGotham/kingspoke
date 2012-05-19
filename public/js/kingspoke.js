34233ca1bb8fdf91debe7dd498495a153da29fef
Adam Bailin <abailin@gmail.com>
2012-05-19 16:25:15 -0500

    css tweaks, js placeholder for ajax call

--- public/js/kingspoke.js
+++ public/js/kingspoke.js
@@ -1,20 +1,51 @@
 var KS = {
 	
-	api_url: "",
+	api_url: "/api",
 	
 	init: function() {
-		log("initialized!");
+		log("ready");
 		
 		KS.init_ui();
 	},
 	
 	init_ui: function() {
+		// focus q1
 		$("#q1 textarea").focus();
 		
+		// ASK click handler
 		$("#btn-ask").click(function(e) {
+
+			var q1 = $("#q1 textarea").text();
+			var q2 = $("#q2 textarea").text();
+			
+			var postdata = {
+				method: "",
+				q1: q1,
+				q2: q2
+			};
+			
+			KS.api_ask(postdata, function(response) {
+				log("got response");
+				log(response);
+			});
 			
 			e.preventDefault();
 		});
+	},
+	
+	api_ask: function(postdata, callback) {
+
+		$.ajax({
+			type: "POST",
+			dataType: "json",
+			url: KS.api_url,
+			data: postdata,
+			success: function(response) {
+				if (callback)
+					callback(response);
+			}
+		});
+		
 	}
 	
 
