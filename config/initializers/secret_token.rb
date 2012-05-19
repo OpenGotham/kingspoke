5d8e1e9a47ef5d0e9d7e60369223fd540350c27b
Matthew Jording <mjording@opengotham.com>
2012-05-19 15:10:32 -0500

    initial barebones rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kingspoke::Application.config.secret_token = 'a1efb230cf561217fc30de55510f662341cfab271927e5cc7c675e765cd62b0c85d7b70f9754225907663c8a805d23d17f33edc56521bacf9367d3380122fdcd'
