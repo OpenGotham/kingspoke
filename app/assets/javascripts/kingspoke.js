05f201af1e174452ff8ee0a5ef13513cb0d5e7a9
Adam Bailin <abailin@gmail.com>
2012-05-20 02:06:17 -0500

    working! weee

--- app/assets/javascripts/kingspoke.js
+++ app/assets/javascripts/kingspoke.js
@@ -32,7 +32,12 @@ var KS = {
   init_ui: function() {
     // focus q1
     $("#option_1").focus();
-
+		
+		$(".why-button").click(function(e) {
+			$(".why-info").slideToggle();
+			e.preventDefault();
+		});
+		
     // ASK click handler
     $("form").submit(function(e) {
 
@@ -55,7 +60,27 @@ var KS = {
       
       KS.api_ask(url, postdata, function(response) {
       	KS.ui_changemode("decision");
+        
         $(".answer").text(response['answer']);
+        
+        var sentiments = response['sentiments'];
+        var i=0;
+        var sentiment1_name, sentiment1_value;
+        var sentiment2_name, sentiment2_value;
+        for (k in sentiments) {
+        	if (i==0) {
+        		sentiment1_name = k;
+        		sentiment1_value = sentiments[k];
+        	}
+        	else {
+        		sentiment2_name = k;
+        		sentiment2_value = sentiments[k];
+        	}
+        	i++;
+        }
+        
+        $(".why-info .choice1").text(sentiment1_name);
+        $(".why-info .choice2").text(sentiment2_name);
       });
 
       e.preventDefault();
